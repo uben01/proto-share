07fe0b4061da47bc2f62ec8f9e664147c3e57dd9
Bence Uto <bence.uto@emarsys.com>
2024-08-20 22:57:30 +0200

    test(language): add missing test

---
+++ src/config/language/language_test.go
@@ -0,0 +1,43 @@
+package language
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const mockLanguageName Name = "myLanguage"
+
+func mockLanguageDefault() *Language {
+	return &Language{
+		ModulePathTemplate:   "myModulePathTemplate",
+		SubDir:               "mySubDir",
+		ProtocCommand:        "myProtocCommand",
+		AdditionalParameters: map[string]string{"myKey": "myValue"},
+	}
+}
+
+var originalMapping = defaultMapping
+var mockMapping = map[Name]*Language{
+	mockLanguageName: mockLanguageDefault(),
+}
+
+func TestMergeWithDefault_NoLanguageMapping_ReturnsError(t *testing.T) {
+	defaultMapping = map[Name]*Language{}
+	defer func() { defaultMapping = originalMapping }()
+
+	_, err := MergeWithDefault(mockLanguageName, nil)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "unsupported language: mylanguage", err.Error())
+}
+
+func TestMergeWithDefault_NoActualLanguage_ReturnsDefaultLanguage(t *testing.T) {
+	defaultMapping = mockMapping
+	defer func() { defaultMapping = originalMapping }()
+
+	merged, err := MergeWithDefault(mockLanguageName, nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, mockLanguageDefault(), merged)
+}
