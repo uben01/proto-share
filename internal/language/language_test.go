1625337954a0cff59b546851edd954d04ab4675f
Bence Uto <bence.uto@emarsys.com>
2024-08-30 18:32:34 +0200

    test(language): add test case

--- internal/language/language_test.go
+++ internal/language/language_test.go
@@ -6,38 +6,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const mockLanguageName Name = "myLanguage"
+const stubLanguageName Name = "myLanguage"
 
-func mockLanguageDefault() *Language {
-	return &Language{
-		ModuleTemplatePath:   "myModulePathTemplate",
-		SubDir:               "mySubDir",
-		ProtocCommand:        "myProtocCommand",
-		AdditionalParameters: map[string]string{"myKey": "myValue"},
-	}
+var stubLanguageDefault = &Language{
+	ModuleTemplatePath:   "myModulePathTemplate",
+	SubDir:               "mySubDir",
+	ProtocCommand:        "myProtocCommand",
+	AdditionalParameters: map[string]string{"myKey": "myValue"},
 }
 
 var originalMapping = defaultMapping
-var mockMapping = map[Name]*Language{
-	mockLanguageName: mockLanguageDefault(),
+var stubMapping = map[Name]*Language{
+	stubLanguageName: stubLanguageDefault,
 }
 
 func TestMergeWithDefault_NoLanguageMapping_ReturnsError(t *testing.T) {
-	defaultMapping = map[Name]*Language{}
-	defer func() { defaultMapping = originalMapping }()
-
-	_, err := MergeWithDefault(mockLanguageName, nil)
+	_, err := MergeWithDefault(stubLanguageName, nil)
 
 	assert.NotNil(t, err)
 	assert.Equal(t, "unsupported language: mylanguage", err.Error())
 }
 
 func TestMergeWithDefault_NoActualLanguage_ReturnsDefaultLanguage(t *testing.T) {
-	defaultMapping = mockMapping
-	defer func() { defaultMapping = originalMapping }()
+	defer setStubMapping()()
 
-	merged, err := MergeWithDefault(mockLanguageName, nil)
+	merged, err := MergeWithDefault(stubLanguageName, nil)
 
 	assert.Nil(t, err)
-	assert.Equal(t, mockLanguageDefault(), merged)
+	assert.Equal(t, stubLanguageDefault, merged)
+}
+
+func TestMergeWithDefault_PartialLanguageGiven_ReturnsMergedLanguage(t *testing.T) {
+	defer setStubMapping()()
+
+	actualLanguage := &Language{
+		SubDir: "actualSubDir",
+	}
+
+	merged, err := MergeWithDefault(stubLanguageName, actualLanguage)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "actualSubDir", merged.SubDir)
+	assert.Equal(t, "myModulePathTemplate", merged.ModuleTemplatePath)
+	assert.Equal(t, "myProtocCommand", merged.ProtocCommand)
+	assert.Equal(t, map[string]string{"myKey": "myValue"}, merged.AdditionalParameters)
+}
+
+func setStubMapping() func() {
+	originalMapping = defaultMapping
+	defaultMapping = stubMapping
+	return func() { defaultMapping = originalMapping }
 }
