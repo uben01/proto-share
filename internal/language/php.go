649576e075577deb52d8990b3951277f836def36
Bence Uto <bence.uto@emarsys.com>
2024-09-27 13:12:11 +0200

    fix(php): fix php paths and defaults

--- internal/language/php.go
+++ internal/language/php.go
@@ -3,11 +3,11 @@ package language
 func defaultPHP() *Language {
 	return &Language{
 		ModuleTemplatePath: "{{ .Module.Name | snakeCase }}",
-		ModuleCompilePath:  "{{ .Module.Name | snakeCase }}",
+		ModuleCompilePath:  "{{ .Module.Name | snakeCase }}/src",
 		SubDir:             "php",
 		ProtocCommand:      "php_out",
 		AdditionalParameters: map[string]string{
-			"phpVersion": "8.1",
+			"phpVersion": "^8",
 			"vendor":     "",
 			"moduleName": "{{ .Module.Name | kebabCase }}",
 		},
